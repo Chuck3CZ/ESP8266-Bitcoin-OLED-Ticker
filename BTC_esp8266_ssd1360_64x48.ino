605675cbefb47def253739d572afced7c6cf91dc
Martin Gabrhel <31049131+Chuck3CZ@users.noreply.github.com>
2022-11-09 22:06:33 +0100

    Version 0.9.2

--- BTC_esp8266_ssd1360_64x48.ino
+++ BTC_esp8266_ssd1360_64x48.ino
@@ -1,17 +1,13 @@
+/*********************************************************************//*
+  This is a Minimalist ESP8266 Oled Bitcoin price ticker
+  from site:
+  https://github.com/Chuck3CZ/esp8266-oled-bitcoin-ticker
 
+  This project is for ESP8266 with 64x48 size OLED display using I2C to communication
+  3 pins are required to interface (2 I2C and one reset)
 
-/*********************************************************************
-This is a Minimalist ESP8266 Oled Bitcoin price ticker 
-
-from site:
-https://github.com/Chuck3CZ/esp8266-oled-bitcoin-ticker
-
-This project is for ESP8266 with 64x48 size OLED display using I2C to communication 
-3 pins are required to interface (2 I2C and one reset)
-
-Written by Martin "Chuck3CZ" Gabrhel
-*********************************************************************/
-
+  Written by Martin "Chuck3CZ" Gabrhel
+/*********************************************************************/
 
 #include "ESP8266WiFi.h"
 #include <ESP8266WebServer.h>
@@ -26,55 +22,51 @@ Written by Martin "Chuck3CZ" Gabrhel
 WiFiServer server(80);
 String header;
 
- // SCL GPIO5 
- // SDA GPIO4
-
-#define OLED_RESET 0  // GPIO0 
+                      // SCL GPIO5
+                      // SDA GPIO4
+#define OLED_RESET 0  // GPIO0
 Adafruit_SSD1306 display(OLED_RESET);
 
- const char* ssid     = "YOUR_WIFI_SSID";
- const char* password = "YOUR_WIFI_PASSWORD";
- const char* host = "api.coindesk.com";
+const char* ssid     = "Matty_Network";
+const char* password = "7312314196";
+const char* host     = "api.coindesk.com";
 
 void setup()   {
   Serial.begin(9600);
 
   WiFi.begin();
 
-
   // by default, generating the high voltage from the 3.3v line internally! (neat!)
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 64x48)
   // init done
-  
+
   // Show image buffer on the display hardware. (Bitcoin logo) -- in adafruit Library
   // internally, this will display the splashscreen
   display.display();
-  delay(2000); 
+  delay(2000);
 
   // Clear the buffer.
   display.clearDisplay();
-  
+
   // init text display
-  
-  // display.setTextColor(BLACK, WHITE); // 'inverted' text
 
+      // display.setTextColor(BLACK, WHITE); // 'inverted' text
 
-  
+  display.setTextSize(1);                //velikost textu 1
+  display.setTextColor(WHITE);           //barva textu
+  display.setCursor(0, 0);               //pozice číslo 1
+  display.println("  BITCOIN   TICKER"); //nadpis
+  display.println();                     //mezera(případně vyzkoušet"\n")
+  display.setTextSize(1);                //velikost textu 2
+  display.setCursor(2, 24);              //pozice číslo 2
+  display.println("88888");              //tisk textu
+  display.display();                     //funkce zapsat do displaye
+  delay(2000);                           //čekat 2s 
+  display.clearDisplay();                //vyčisti display pro další funkci
+
   display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0,0);
+  display.setCursor(0, 0);
   display.println("  BITCOIN   TICKER");
-  display.println();
-  display.setTextSize(2);
-  display.setCursor(2,24);
-  display.println("88888");
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-  display.setTextSize(1);
-  display.setCursor(0,0);
- display.println("  BITCOIN   TICKER");
   display.setTextSize(2);
   display.println("     ");
   display.display();
@@ -89,53 +81,53 @@ void setup()   {
 
 
   WiFi.begin(ssid, password);
-  
-  
-  while (WiFi.status() != WL_CONNECTED) 
+
+
+  while (WiFi.status() != WL_CONNECTED)
   {
     Serial.print(".");
-  delay(50);
+    delay(50);
     Serial.print(".");
-  delay(50);
+    delay(50);
     Serial.print(".");
-  
-// Main text
-   display.setTextSize(1);
-   display.setCursor(0,0);
-   display.println("  BITCOIN   TICKER");
-   display.display();
- delay(500);
-    
-// Loading Dot 1    
-   display.setCursor(0,24); 
-   display.setTextSize(2);
-   display.println(" .  ");
-   display.display();
- delay(500);
-    
-// Loading Dot 2       
-   display.setCursor(0,24);
-   display.println("  .  ");
-   display.display();
- delay(500);
-        
-// Loading Dot 3   
-   display.setCursor(0,24);
-   display.println("   .  ");
-   display.display();
- delay(500);
- 
-  display.clearDisplay();
+
+    // Main text
+    display.setTextSize(1);
+    display.setCursor(0, 0);
+    display.println("  BITCOIN   TICKER");
+    display.display();
+    delay(500);
+
+    // Loading Dot 1
+    display.setCursor(0, 24);
+    display.setTextSize(2);
+    display.println(" .  ");
+    display.display();
+    delay(500);
+
+    // Loading Dot 2
+    display.setCursor(0, 24);
+    display.println("  .  ");
+    display.display();
+    delay(500);
+
+    // Loading Dot 3
+    display.setCursor(0, 24);
+    display.println("   .  ");
+    display.display();
+    delay(500);
+
+    display.clearDisplay();
   }
 
   display.setTextSize(1);
-  display.setCursor(0,0);
+  display.setCursor(0, 0);
   display.println("  BITCOIN   TICKER");
-  display.setCursor(0,24);
+  display.setCursor(0, 24);
   display.println(" Connected");
   display.display();
-  
-  
+
+
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
@@ -146,28 +138,28 @@ void setup()   {
 
 
 void loop() {
-   
+
   WiFiClient client;
   const int httpPort = 80;
   if (!client.connect(host, httpPort)) {
     return;
   }
-  
+
   String url = "/v1/bpi/currentprice.json";
-  
+
   client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" + 
+               "Host: " + host + "\r\n" +
                "Connection: close\r\n\r\n");
   delay(100);
-  
+
 
   String answer;
-  while(client.available()){
+  while (client.available()) {
     String line = client.readStringUntil('\r');
     answer += line;
   }
   client.stop();
-  
+
   String jsonAnswer;
   int jsonIndex;
 
@@ -185,34 +177,36 @@ void loop() {
   // Obtaining prices like float value
   int rateIndex = jsonAnswer.indexOf("rate_float");
   String priceString = jsonAnswer.substring(rateIndex + 12, rateIndex + 18);
-    priceString.trim();
+  priceString.trim();
   float price = priceString.toFloat();
 
-    Serial.println();
-    Serial.println("Bitcoin price: ");
-    Serial.println(price);
+  Serial.println();
+  Serial.println("Bitcoin price: ");
+  Serial.println(price);
 
-  if(price > 0){
-    display.clearDisplay();  
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0,0);
-  display.println("  BITCOIN   TICKER");
-  display.println();
-  display.setCursor(2,24);
-  display.setTextSize(2);
-  display.println(priceString);
-  display.display();
-  delay(5000);
+
+//kontrola ceny nad 0 
+  if (price > 0) {
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 0);
+    display.println("  BITCOIN   TICKER");
+    display.println();
+    display.setCursor(2, 24);
+    display.setTextSize(2);
+    display.println(priceString);
+    display.display();
+    delay(5000);
   }
-  
-  if(price = 0){
-    display.clearDisplay(); 
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0,0);
-  display.println(" BITCOIN   TICKER");
-  display.println(" Price     ERROR");
-  delay(5000);
+
+  if (price = 0) {
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 0);
+    display.println(" BITCOIN   TICKER");
+    display.println(" Price     ERROR");
+    delay(5000);
   }
 }
